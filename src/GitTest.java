dc8edd57deb15e64d2aa9c084bd0ef7dc3d85181
lifaqiang15 <lifaqiang15@163.com>
2022-02-15 11:28:01 +0800

    third commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git1");
         System.out.println("Hello git2");
+        System.out.println("Hello git3");
     }
 }
